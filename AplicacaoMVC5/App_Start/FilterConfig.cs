17627134ac9a3ad15e7010513d726cd714b4f49b
Samory Sundjata <samory.sundjata@gmail.com>
2020-12-07 17:29:00 -0300

    Add project files.

---
+++ AplicacaoMVC5/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacaoMVC5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
