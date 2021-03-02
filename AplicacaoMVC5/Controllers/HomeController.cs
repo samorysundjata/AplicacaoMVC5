96ddf2a1ee8786c2cf4fdce41ea20b5ad24e20a8
Samory Sundjata <samory.sundjata@gmail.com>
2021-03-02 17:46:07 -0300

    Action Result.

--- AplicacaoMVC5/Controllers/HomeController.cs
+++ AplicacaoMVC5/Controllers/HomeController.cs
@@ -1,30 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace AplicacaoMVC5.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        #region Public Methods
+
+        public ViewResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        #endregion Public Methods
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
