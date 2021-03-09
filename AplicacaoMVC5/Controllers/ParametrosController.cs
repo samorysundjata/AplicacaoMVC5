8b72b3992dc1f6b8f330d5e15dd269081139be40
Samory Sundjata <samory.sundjata@gmail.com>
2021-03-09 09:00:58 -0300

    Rotas com parâmetros

---
+++ AplicacaoMVC5/Controllers/ParametrosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacaoMVC5.Controllers
+{
+    public class ParametrosController : Controller
+    {
+        // GET: Parametros
+        public ActionResult Index(int id1)
+        {
+            return View();
+        }
+    }
+}
---
+++ AplicacaoMVC5/Controllers/TesteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacaoMVC5.Controllers
+{
+    [RoutePrefix("testes")]
+    public class TesteController : Controller
+    {
+        [Route("{id2:int}")]
+        public ActionResult IndexTeste(int id2, string texto)
+        {
+            return View();
+        }
+
+        [Route("{texto:maxlength(50)}/um-outro-teste/{id2:int}")]
+        public ActionResult IndexTeste2(int id2, string texto)
+        {
+            return View("IndexTeste");
+        }
+    }
+}
