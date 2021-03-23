9b5eed1260b75b88c2b3999281adf9b283604f7a
Samory Sundjata <samory.sundjata@gmail.com>
2021-03-23 08:53:11 -0300

    Comentando na AlunoController

--- AplicacaoMVC5/Controllers/AlunoController.cs
+++ AplicacaoMVC5/Controllers/AlunoController.cs
@@ -8,28 +8,28 @@ namespace AplicacaoMVC5.Controllers
     {
         #region Public Methods
 
-        public ActionResult Index(Aluno aluno)
-        {
-            if (!ModelState.IsValid) return View(aluno);
+        //public ActionResult Index(Aluno aluno)
+        //{
+        //    if (!ModelState.IsValid) return View(aluno);
 
-            return View(aluno);
-        }
+        //    return View(aluno);
+        //}
 
-        [Route("novo-aluno")]
-        public ActionResult Novo(Aluno aluno)
-        {
-            aluno = new Aluno
-            {
-                Id = 1,
-                Nome = "Joseh das Couves",
-                CPF = "0126468780-98",
-                DataMatricula = DateTime.Now,
-                Email = "jose@couves.com",
-                Ativo = true
-            };
+        //[Route("novo-aluno")]
+        //public ActionResult Novo(Aluno aluno)
+        //{
+        //    aluno = new Aluno
+        //    {
+        //        Id = 1,
+        //        Nome = "Joseh das Couves",
+        //        CPF = "0126468780-98",
+        //        DataMatricula = DateTime.Now,
+        //        Email = "jose@couves.com",
+        //        Ativo = true
+        //    };
 
-            return RedirectToAction("Index", aluno);
-        }
+        //    return RedirectToAction("Index", aluno);
+        //}
 
         #endregion Public Methods
     }
