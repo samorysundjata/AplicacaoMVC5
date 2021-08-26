c6a0869b6472d56e5b588635831173e67dba8860
Samory Sundjata <samory.sundjata@gmail.com>
2021-08-26 07:58:55 -0300

    EF inicial

---
+++ AplicacaoMVC5/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AplicacaoMVC5.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AplicacaoMVC5.Data
+{
+    public class AppDbContext : DbContext
+    {
+        #region Public Constructors
+
+        public AppDbContext() : base("DefaultConnection")
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        #endregion Public Properties
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Aluno>().ToTable("Alunos");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
